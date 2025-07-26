91c2dfd0e99ea47b72643a0c339d037ee284423a
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-07-26 14:57:19 +0000

    const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwOm6gXcBzY

--- src/components/dft-machine/DftMachineLogManagerDialog.tsx
+++ src/components/dft-machine/DftMachineLogManagerDialog.tsx
@@ -98,8 +98,8 @@ export default function DftMachineLogManagerDialog({ isOpen, setIsOpen, machine 
         
             const res = await fetch(WEB_APP_URL, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
+                 headers: {
+                    'Content-Type': 'text/plain;charset=utf-8',
                 },
                 body: JSON.stringify(payload),
             });
--- src/components/ut-machine/UTMachineLogManagerDialog.tsx
+++ src/components/ut-machine/UTMachineLogManagerDialog.tsx
@@ -100,7 +100,7 @@ export default function UTMachineLogManagerDialog({ isOpen, setIsOpen, machine }
             const res = await fetch(WEB_APP_URL, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'text/plain;charset=utf-8',
                 },
                 body: JSON.stringify(payload),
             });
