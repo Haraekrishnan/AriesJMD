16cab03a32f1f6145f905c19791c5452fb2195ba
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-08-17 11:09:49 +0000

    Change the recepiant mail id to harikrishnan.bornagain@gmail.com

--- src/app/actions/sendPpeRequestEmail.ts
+++ src/app/actions/sendPpeRequestEmail.ts
@@ -1,4 +1,3 @@
-
 'use server';
 
 import * as nodemailer from 'nodemailer';
@@ -90,7 +89,7 @@ export async function sendPpeRequestEmail(ppeData: Record<string, any>) {
   try {
     await transporter.sendMail({
       from: `"Aries PPE Request" <${GMAIL_USER}>`,
-      to: 'vijay.sai@ariesmar.com',
+      to: 'harikrishnan.bornagain@gmail.com',
       subject: subject,
       html: htmlBody,
     });
