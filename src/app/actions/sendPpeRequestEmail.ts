92f231d529db7ad72f9a06a0981ebf14ea125364
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-08-18 14:31:17 +0000

    change recepiant mail in sendpperequestemail.ts file as harikrishnan.bor

--- src/app/actions/sendPpeRequestEmail.ts
+++ src/app/actions/sendPpeRequestEmail.ts
@@ -93,7 +93,7 @@ export async function sendPpeRequestEmail(ppeData: Record<string, any>) {
 
     await transporter.sendMail({
       from: `"Aries PPE Request" <${GMAIL_USER}>`,
-      to: 'vijay.sai@ariesmar.com',
+      to: 'harikrishnan.bornagain@gmail.com',
       subject: subject,
       html: htmlBody,
     });
@@ -109,7 +109,7 @@ export async function sendPpeRequestEmail(ppeData: Record<string, any>) {
     try {
       await resend.emails.send({
         from: `Aries PPE Request <${GMAIL_USER}>`,
-        to: 'vijay.sai@ariesmar.com',
+        to: 'harikrishnan.bornagain@gmail.com',
         subject: subject,
         html: htmlBody,
       });
@@ -121,4 +121,3 @@ export async function sendPpeRequestEmail(ppeData: Record<string, any>) {
     }
   }
 }
-
