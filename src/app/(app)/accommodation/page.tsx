e84469fcc6f1db6c7eba2e28dc6f3c64f922cc1a
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-08-19 06:59:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/accommodation/page.tsx
+++ src/app/(app)/accommodation/page.tsx
@@ -120,3 +120,7 @@ export default function AccommodationPage() {
                     setIsOpen={setIsEditBuildingOpen}
                     building={selectedBuilding}
                 />
+            )}
+        </div>
+    );
+}
