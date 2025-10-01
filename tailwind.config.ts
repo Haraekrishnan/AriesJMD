7d63b321a72edbc867fb9264d733a5a9b801c05c
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-10-01 05:12:50 +0000

    COLORING IS NOT COMING

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/job-codes.ts', // Make sure tailwind scans this file for classes
   ],
   theme: {
     extend: {
