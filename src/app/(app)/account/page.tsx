e8ccc12628431c44b161d5f17c23502ff507a247
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-10-13 04:16:24 +0000

    Try fixing this error: `Console Error: Each child in a list should have

--- src/app/(app)/account/page.tsx
+++ src/app/(app)/account/page.tsx
@@ -289,10 +289,10 @@ export default function AccountPage() {
                       </TableRow>
                   </TableHeader>
                   <TableBody>
-                      {visibleUsers.map(report => {
+                      {visibleUsers.map((report, index) => {
                           const supervisor = users.find(u => u.id === report.supervisorId);
                           return (
-                            <TableRow key={report.id}>
+                            <TableRow key={`${report.id}-${index}`}>
                                 <TableCell>
                                     <div className="flex items-center gap-3">
                                         <Avatar className="h-9 w-9">
