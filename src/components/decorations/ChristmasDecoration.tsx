ab92b54d93972ca18cd78babc81c560343c180b0
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-12-25 03:10:03 +0000

    add some santa clause, his vehicle , snow man, christmas tree also

--- src/components/decorations/ChristmasDecoration.tsx
+++ src/components/decorations/ChristmasDecoration.tsx
@@ -5,46 +5,16 @@ import './snow.css';
 export default function ChristmasDecoration() {
     return (
         <div className="snow-container">
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
-            <div className="snow"></div>
+            {/* Snowflakes */}
+            {Array.from({ length: 40 }).map((_, i) => (
+                <div key={`snow-${i}`} className="snow"></div>
+            ))}
+            
+            {/* Decorations */}
+            <div className="decoration sleigh">🎅&#129420;</div>
+            <div className="decoration tree tree-1">🎄</div>
+            <div className="decoration tree tree-2">🎄</div>
+            <div className="decoration snowman">⛄</div>
         </div>
     );
 }
