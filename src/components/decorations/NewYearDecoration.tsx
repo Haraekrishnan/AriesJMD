f45d7101f52445a9474d1568ca3ad8312d46e494
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-12-25 11:42:59 +0000

    make the flying objects /animations and baloons colors to be same as in

--- src/components/decorations/NewYearDecoration.tsx
+++ src/components/decorations/NewYearDecoration.tsx
@@ -5,22 +5,19 @@ import './new-year.css';
 export default function NewYearDecoration() {
     return (
         <div className="new-year-container">
-            {/* Fireworks */}
-            <div className="firework"></div>
-            <div className="firework"></div>
-            <div className="firework"></div>
-            <div className="firework"></div>
-            <div className="firework"></div>
+            {/* --- BIG CIRCLES --- */}
+            <div className="balloon balloon-1"></div>
+            <div className="balloon balloon-2"></div>
+            <div className="balloon balloon-3"></div>
+            
+            {/* --- CONFETTI --- */}
+            {Array.from({ length: 10 }).map((_, i) => (
+                <div key={`confetti-${i}`} className="firework"></div>
+            ))}
 
             <div className="happy-new-year">
                 <h1>Happy New Year 2026</h1>
             </div>
-
-            {/* Balloons */}
-            <div className="balloon balloon-1">🎈</div>
-            <div className="balloon balloon-2">🎉</div>
-            <div className="balloon balloon-3">🎈</div>
-            <div className="balloon balloon-4">🎊</div>
         </div>
     );
 }
