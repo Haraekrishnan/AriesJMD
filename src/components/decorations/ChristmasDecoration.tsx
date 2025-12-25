d838280e27ddd67ad4141544c02a93ec0ab48a44
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-12-25 04:28:40 +0000

    remove these red balls its not looking good, make some other elements in

--- src/components/decorations/ChristmasDecoration.tsx
+++ src/components/decorations/ChristmasDecoration.tsx
@@ -49,9 +49,9 @@ export default function ChristmasDecoration() {
             <div className="star star2">⭐</div>
             <div className="star star3">✨</div>
             <div className="star star4">⭐</div>
-            <div className="ball ball1"></div>
-            <div className="ball ball2"></div>
-            <div className="ball ball3"></div>
+            <div className="star star5">✨</div>
+            <div className="star star6">⭐</div>
+            <div className="star star7">✨</div>
         </div>
     );
 }
