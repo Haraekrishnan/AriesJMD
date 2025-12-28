6609a9b47bf54e13619c727bbabd44dc83520da9
Harikrishnan P S <haraekrishnanplr@gmail.com>
2025-12-28 15:03:15 +0000

    still no idnicatory for damage reports page

--- src/app/(app)/store-inventory/page.tsx
+++ src/app/(app)/store-inventory/page.tsx
@@ -206,6 +206,7 @@ export default function StoreInventoryPage() {
                     <Button asChild variant="outline"><Link href="/ppe-stock"><Package className="mr-2 h-4 w-4"/> PPE Stock</Link></Button>
                     <Button asChild variant="outline"><Link href="/igp-ogp"><ArrowRightLeft className="mr-2 h-4 w-4"/> IGP/OGP Register</Link></Button>
                     <Button asChild variant="outline"><Link href="/tp-certification"><FileText className="mr-2 h-4 w-4"/> TP Cert Lists</Link></Button>
+                    
                     {can.manage_inventory && (
                         <Button asChild variant="outline" className="relative">
                             <Link href="/damage-reports">
@@ -382,3 +383,4 @@ export default function StoreInventoryPage() {
         </div>
     );
 }
+
