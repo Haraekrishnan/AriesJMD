ddc4365172b0cfaaaa027c790763d61482b129a0
Harikrishnan P S <haraekrishnanplr@gmail.com>
2026-02-01 15:06:34 +0000

    Try fixing this error: `Runtime ReferenceError: useFieldArray is not def

--- src/components/job-progress/CreateJobDialog.tsx
+++ src/components/job-progress/CreateJobDialog.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { useMemo } from 'react';
